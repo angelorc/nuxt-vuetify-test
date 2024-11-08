4ff4b1fc99f8e84e1749fedec76115770d662172
angelorc <angelo@rcstart.it>
2024-11-08 12:58:09 +0100

    Add SCSS settings and update Nuxt configuration for Vuetify integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,43 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  
   devtools: { enabled: true },
+  
   modules: ['vuetify-nuxt-module'],
+  
   css: ['~/assets/scss/style.scss'],
+  
   vuetify: {
     moduleOptions: {
+      disableVuetifyStyles: true,
+      
       styles: {
-        configFile: '/assets/scss/style.scss'
+        configFile: '/assets/scss/settings.scss'
+      },
+
+      includeTransformAssetsUrls: {
+        NuxtImg: ['src'],
+        OgImage: ['image'],
+      },
+
+      ssrClientHints: {
+        reloadOnFirstRequest: false,
+        prefersColorScheme: true,
+        // prefersColorSchemeOptions: {
+        //   useBrowserThemeOnly: false,
+        // },
+        viewportSize: true,
+      },
+    }
+  },
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-api',
+        }
       }
     }
   }
---
+++ vuetify.config.ts
@@ -0,0 +1,6 @@
+import { md3 } from 'vuetify/blueprints'
+import { defineVuetifyConfiguration } from 'vuetify-nuxt-module/custom-configuration'
+
+export default defineVuetifyConfiguration({
+  blueprint: md3,
+})
